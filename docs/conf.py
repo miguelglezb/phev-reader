e8461eba815d8856495ed1bf96087fb20471b06b
miguelglezb <miguelgb@astro.unam.mx>
2025-03-09 11:27:35 +1100

    Setup documentation for readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+# Add the project's root directory to sys.path
+sys.path.insert(0, os.path.abspath(".."))
+
+# Project information
+project = 'My Project'
+copyright = '2025, My Name'
+author = 'My Name'
+release = '1.0'
+
+# General configuration
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autodoc_mock_imports = []  # Add any modules that shouldn't be imported
+
+# HTML output options
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+def setup(app):
+    app.add_css_file('custom.css')  # Optional: Add custom CSS styling
