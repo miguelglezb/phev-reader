1b921419ad2e7e7af6e985c982468bd3ab0096b4
miguelglezb <miguelgb@astro.unam.mx>
2025-03-09 20:33:24 +1100

    Cleanup for documentation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="phev-reader",  # Adjust if needed
+    version="0.1.0",
+    packages=find_packages(),  # Automatically find submodules
+    install_requires=["pandas","numpy"],  # Add dependencies here
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Adjust if needed
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
